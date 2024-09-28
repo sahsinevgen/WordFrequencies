36f55d354a863502df1e67a809c6268f47bc4f8d
Alexander Sinelnikov <malinuskeshar@gmail.com>
2024-09-28 15:50:48 +0400

    add t_solution_func type

--- src/solutions/solutions.h
+++ src/solutions/solutions.h
@@ -3,11 +3,15 @@
 
 #include <string>
 
+
 #include "std_solution/std_solution.h"
 #include "trie_solution/trie_solution.h"
 
 
-void (*solutions[])(std::string, std::string) =  {
+typedef void (*t_solution_func)(std::string, std::string);
+
+
+t_solution_func solutions[] =  {
     std_solution::solution,
     trie_solution::solution,
 };
